7d644ebc8e2008b215902d2b96e23cc8803c14c3
jade-d-cutler <153133130+jade-d-cutler@users.noreply.github.com>
2024-10-11 14:32:53 -0700

    Add files via upload

---
+++ Readme.txt
@@ -0,0 +1,10 @@
+== Jade Cutler Portfolio Website
+
+Description: 
+This website was made so that I could display my projects, skills, and a link to my GitHub. 
+
+Functionality:
+At the top if the page is a greeting inside a carousel. You can click the GitHub buitton to be brought to my account. You can also click my resume button to be brought to my Overleaf.com resume. 
+You can scroll through the web page to see all the contents. Once at the bottom there is a scrollup button that can be clicked to bring you to the top again.
+At the bottom of the page is my contact information and a contact form. 
+The contact form isn't fully functional, I just wanted to display that I could add a form and make it functional if needed. 
---
+++ assets/js/main.js
@@ -0,0 +1,88 @@
+"use strict";
+
+
+jQuery(document).ready(function ($) {
+
+
+
+    /*---------------------------------------------*
+     * STICKY scroll
+     ---------------------------------------------*/
+
+    $.localScroll();
+
+
+
+    /*---------------------------------------------*
+     * WOW
+     ---------------------------------------------*/
+
+    var wow = new WOW({
+        mobile: false // trigger animations on mobile devices (default is true)
+    });
+    wow.init();
+
+
+
+
+
+
+
+    /* ---------------------------------------------------------------------
+     Carousel
+     ---------------------------------------------------------------------= */
+
+    $('.main_home_slider').owlCarousel({
+        responsiveClass: true,
+        autoplay: false,
+        items: 1,
+        loop: true,
+        dots: true,
+        nav: false,
+        navText: [
+            "<i class='lnr lnr-chevron-left'></i>",
+            "<i class='lnr lnr-chevron-right'></i>"
+        ],
+        autoplayHoverPause: true
+
+    });
+
+
+
+
+// main-menu-scroll
+
+    jQuery(window).scroll(function () {
+        var top = jQuery(document).scrollTop();
+        var height = 300;
+     
+    });
+
+// scroll Up
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 600) {
+            $('.scrollup').fadeIn('slow');
+        } else {
+            $('.scrollup').fadeOut('slow');
+        }
+    });
+
+    $('.scrollup').click(function () {
+        $("html, body").animate({scrollTop: 0}, 1000);
+        return false;
+    });
+
+
+
+
+
+    //End
+});
+
+
+
+
+
+
+
